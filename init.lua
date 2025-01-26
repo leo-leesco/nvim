2a39b606db0030a9c161ae94d6b98d0591f2a50e
Léo LEESCO <leo.leesco@gmail.com>
2025-01-26 13:00:13 +0100

    compile with bibliography

--- init.lua
+++ init.lua
@@ -593,7 +593,19 @@ require('lazy').setup({
                 }
               }
             }
-          }
+          },
+
+          sqlls = {}
+          -- postgres_lsp = {
+          --   default_config = {
+          --     name = 'postgres_lsp',
+          --     cmd = { 'postgres_lsp' },
+          --     filetypes = { 'sql' },
+          --     single_file_support = true,
+          --     root_dir = require('lspconfig.util').root_pattern 'root-file.txt',
+          --   },
+          -- }
+
           -- clangd = {},
           -- gopls = {},
           -- pyright = {},
@@ -997,8 +1009,13 @@ vim.api.nvim_create_autocmd("BufWritePost", {
   group = latex_group,
   pattern = "*.tex",
   callback = function()
-    vim.cmd("silent !pdflatex -shell-escape %")
-    vim.cmd("silent! !open %:r.pdf")
+    vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%'")
+    vim.cmd("silent !bibtex '%:r'")
+    vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%'")
+    vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%'")
+    vim.cmd("silent! !open '%:r.pdf'")
+    -- vim.cmd("silent! !open -g '%:r.pdf'")
+    -- vim.cmd [[ !osascript -e 'tell application "Preview" to open POSIX file "%:p:r.pdf"' -e 'tell application "Preview" to activate' -e 'tell application "System Events" to tell process "Preview" to keystroke "1" using {command down}' -e 'tell application "System Events" to tell process "Preview" to keystroke "9" using {command down}' -e 'tell application "System Events" to tell process "Preview" to key code 31 using {command down}' -e 'tell application "System Events" to tell process "Preview" to key code 124 using {command down}' ]]
     vim.cmd("silent! !open -a wezterm.app")
   end,
 })
