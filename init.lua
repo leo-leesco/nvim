7b79ada70cb7eb24c6f53a3bf088afe1012c8c4f
Léo LEESCO <leo.leesco@gmail.com>
2025-02-06 22:25:50 +0100

    isolate plugin imports

--- init.lua
+++ init.lua
@@ -157,9 +157,6 @@ require('lazy').setup({
     --
     -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-    -- provides the comment commands family (`gcc`, `gc{`, `gc}`, etc.)
-    'numToStr/Comment.nvim',
-
 
     -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
     -- init.lua. If you want these files, they are in the repository, so you can just download them and
---
+++ lua/kickstart/plugins/comments.lua
@@ -0,0 +1,4 @@
+return {
+	-- provides the comment commands family (`gcc`, `gc{`, `gc}`, etc.)
+	'numToStr/Comment.nvim',
+}
