220dc013d07a9e1fc2e2366c4df0185d316c38ec
Léo LEESCO <leo.leesco@gmail.com>
2025-02-13 18:19:01 +0100

    put latex config inside default lazyvim template

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,27 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local latex = vim.api.nvim_create_augroup("Latex", { clear = true })
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = latex,
+  pattern = "*.tex",
+  callback = function()
+    vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%'")
+
+    -- check for bibtex
+    if vim.fn.glob(vim.fn.getcwd() .. "/*.bib") then
+      vim.print(".bib found")
+      vim.cmd("silent !bibtex '%:r'")
+      vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%'")
+      vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%'")
+    end
+
+    -- toggle Preview to make sure the PDF is re-rendered
+    vim.cmd("silent! !open '%:r.pdf'")
+    -- vim.cmd("silent! !open -g '%:r.pdf'")
+    -- vim.cmd [[ !osascript -e 'tell application "Preview" to open POSIX file "%:p:r.pdf"' -e 'tell application "Preview" to activate' -e 'tell application "System Events" to tell process "Preview" to keystroke "1" using {command down}' -e 'tell application "System Events" to tell process "Preview" to keystroke "9" using {command down}' -e 'tell application "System Events" to tell process "Preview" to key code 31 using {command down}' -e 'tell application "System Events" to tell process "Preview" to key code 124 using {command down}' ]]
+    vim.cmd("silent! !open -a wezterm.app")
+  end,
+})
