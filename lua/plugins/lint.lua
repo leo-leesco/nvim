b2bcc2676b77451d7186a0d615d65c542d29b30d
Léo LEESCO <leo.leesco@gmail.com>
2025-03-16 22:30:59 +0100

    remove useless linter for markdown

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,9 +3,6 @@ return {
     "mfussenegger/nvim-lint",
     opts = {
       linters = {
-        markdownlint = {
-          args = { "--disable", "MD013", "MD031", "MD032", "MD033", "--" },
-        },
         ["markdownlint-cli2"] = {
           args = { "--config", "~/.markdownlintrc", "--" },
         },
