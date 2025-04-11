a058c5ce8bd6b6ef7acd8a3484446a6b3df781fe
Léo LEESCO <leo.leesco@gmail.com>
2025-04-11 12:10:43 -0700

    ignore built-in spell checker
    markdown and text only
    we rely on ltex spell checking, which is language aware

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -30,3 +30,10 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     vim.cmd("silent! !open -a wezterm.app")
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "txt" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
