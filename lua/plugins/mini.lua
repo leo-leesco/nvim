2c6a38d76e55e4cf688159f49d83a6776576d57a
Léo LEESCO <leo.leesco@gmail.com>
2025-04-25 11:06:42 -0700

    mini plugins

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,17 @@
+function mini_plugins_from_list(tbl)
+	local result = {}
+	for i, v in ipairs(tbl) do
+		result[i] = {
+			'echasnovski/mini.' .. v,
+			version = '*',
+			config = function()
+				require('mini.' .. v).setup()
+			end
+		}
+	end
+	return result
+end
+
+local plugins = { "pairs", "align", 'ai', "completion", "comment", "snippets", "operators" }
+
+return mini_plugins_from_list(plugins)
