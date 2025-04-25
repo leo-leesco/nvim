3d4446a67170a905e00948a57603f12d867222e6
Léo LEESCO <leo.leesco@gmail.com>
2025-04-24 17:10:07 -0700

    add tree-sitter

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -37,3 +37,15 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
