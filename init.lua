64da5c55dd4a6f366a7f7c047400c62ae0db44e1
Léo LEESCO <leo.leesco@gmail.com>
2025-04-24 17:09:59 -0700

    automatically source config folder

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
-require("config.lazy")
-require("config.autocmds")
-require("config.lsp")
+-- import all files in directory `config`
+for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/config", [[v:val =~ '\.lua$']])) do
+	require("config." .. file:gsub("%.lua$", ""))
+end
