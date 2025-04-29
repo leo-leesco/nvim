7f4ecd802860a3e5cd755a158eed76d2412a1b1f
Léo LEESCO <leo.leesco@gmail.com>
2025-04-29 16:03:34 -0700

    load lazy.nvim first

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+require("config.lazy")
+
 -- import all files in directory `config`
 for _, file in ipairs(vim.fn.readdir(vim.fn.stdpath("config") .. "/lua/config", [[v:val =~ '\.lua$']])) do
 	require("config." .. file:gsub("%.lua$", ""))
