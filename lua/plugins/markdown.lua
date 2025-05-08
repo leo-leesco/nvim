bab45693beb34d26dc3b789c821ccb174af0bdec
Léo LEESCO <leo.leesco@gmail.com>
2025-05-08 11:59:40 -0700

    fix loading of markdown extensions

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -13,6 +13,7 @@ return {
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown" },
 		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
 		---@module 'render-markdown'
 		---@type render.md.UserConfig
