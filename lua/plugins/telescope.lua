03ceef412ed0c5c543b63178e038b66f754abeee
Léo LEESCO <leo.leesco@gmail.com>
2025-05-08 17:16:14 -0700

    fix commands for zoxide built-in vim's cd

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,13 @@
 return {
 	{
+		"jvgrootveld/telescope-zoxide",
+		dependencies = {
+			"nvim-lua/popup.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
@@ -11,12 +19,39 @@ return {
 
 		opts = {
 			extensions = {
+
 				fzf = {
 					fuzzy = true, -- false will only do exact matching
 					override_generic_sorter = true, -- override the generic sorter
 					override_file_sorter = true, -- override the file sorter
 					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 					-- the default case_mode is "smart_case"
+				},
+
+				zoxide = {
+					prompt_title = "[ Walking on the shoulders of TJ ]",
+					mappings = {
+						default = {
+							after_action = function(selection)
+								print("Update to (" .. selection.z_score .. ") " .. selection.path)
+							end,
+						},
+
+						["<C-s>"] = {
+							before_action = function(selection)
+								print("before C-s")
+							end,
+							action = function(selection)
+								vim.cmd.edit(selection.path)
+							end,
+						},
+
+						["<C-q>"] = {
+							action = function(selection)
+								vim.cmd("split " .. selection.path)
+							end,
+						},
+					},
 				},
 			},
 		},
@@ -25,12 +60,15 @@ return {
 			local telescope = require("telescope")
 			telescope.setup(opts)
 			telescope.load_extension("fzf")
+			telescope.load_extension("zoxide")
 
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+			vim.keymap.set("n", "<leader>cd", telescope.extensions.zoxide.list)
 		end,
 	},
 }
