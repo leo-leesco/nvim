d214dc95e2b001272b4ca5d761e988622319b98b
Léo LEESCO <leo.leesco@gmail.com>
2025-05-26 16:44:36 -0700

    update activating lazydev in directories

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,10 +8,19 @@ return {
 		ft = "lua", -- only load on lua files
 		opts = {
 			library = {
+				-- Library paths can be absolute
+				"~/.config/nvim/",
+				-- Or relative, which means they will be resolved from the plugin dir.
+				"lazy.nvim",
 				-- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			},
+			-- always enable unless `vim.g.lazydev_enabled = false`
+			-- This is the default
+			enabled = function(root_dir)
+				return vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled
+			end,
 		},
 	},
 }
