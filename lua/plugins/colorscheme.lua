835a55b869b46968721396554d28bda4fec23c39
Léo LEESCO <leo.leesco@gmail.com>
2025-05-27 10:09:02 -0700

    add integration for catpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,41 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		integrations = {
+			cmp = true,
+			gitsigns = true,
+			nvimtree = true,
+			treesitter = true,
+			mini = {
+				enabled = true,
+				indentscope_color = "",
+			},
+			blink_cmp = true,
+			fzf = true,
+			gitsigns = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+					ok = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+					ok = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
+			lsp_trouble = false,
+		},
+	},
+}
