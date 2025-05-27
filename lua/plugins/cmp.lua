73b25b7e74cf4ae4285d7d9587a11231c768bb6b
Léo LEESCO <leo.leesco@gmail.com>
2025-05-27 16:02:41 -0700

    better cmp ?

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,103 +1,106 @@
 return {
+	-- Built-in snippets with nvim-snippets
 	{
-		"L3MON4D3/LuaSnip",
-		lazy = true,
-		build = "make install_jsregexp",
-		dependencies = {
+		"garymjr/nvim-snippets",
+		keys = {
 			{
-				"rafamadriz/friendly-snippets",
-				config = function()
-					require("luasnip.loaders.from_vscode").lazy_load()
-					require("luasnip.loaders.from_vscode").lazy_load({
-						paths = { vim.fn.stdpath("config") .. "/snippets" },
-					})
+				"<Tab>",
+				function()
+					if vim.snippet.active({ direction = 1 }) then
+						vim.schedule(function()
+							vim.snippet.jump(1)
+						end)
+						return
+					end
+					return "<Tab>"
 				end,
+				expr = true,
+				silent = true,
+				mode = "i",
+			},
+			{
+				"<Tab>",
+				function()
+					vim.schedule(function()
+						vim.snippet.jump(1)
+					end)
+				end,
+				expr = true,
+				silent = true,
+				mode = "s",
+			},
+			{
+				"<S-Tab>",
+				function()
+					if vim.snippet.active({ direction = -1 }) then
+						vim.schedule(function()
+							vim.snippet.jump(-1)
+						end)
+						return
+					end
+					return "<S-Tab>"
+				end,
+				expr = true,
+				silent = true,
+				mode = { "i", "s" },
 			},
 		},
-		opts = {
-			history = true,
-			delete_check_events = "TextChanged",
-		},
-	},
-
-	{
-		"hrsh7th/nvim-cmp",
-		optional = true,
-		dependencies = { "saadparwaiz1/cmp_luasnip" },
-		opts = function(_, opts)
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-
-			-- Snippet expansion - choose ONE method
-			opts.snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			}
-
-			-- Sources for completion
-			opts.sources = cmp.config.sources({
-				{ name = "lazydev", group_index = 0 }, -- lazydev completions first
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "nvim_lua" },
-			}, {
-				{ name = "buffer" },
-			})
-
-			-- Key mappings
-			opts.mapping = cmp.mapping.preset.insert({
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			})
+		config = function()
+			require("vim.snippets").from_vscode("rafamadriz/friendly-snippets")
+			require("vim.snippets").from_vscode(vim.fn.stdpath("config") .. "/snippets")
 		end,
 	},
 
+	-- blink.cmp as the complete completion engine
 	{
 		"saghen/blink.cmp",
-		dependencies = { "rafamadriz/friendly-snippets" },
 		version = "1.*",
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"folke/lazydev.nvim",
+		},
 		opts = {
-			keymap = { preset = "default" },
+			keymap = { preset = "super-tab" },
+
 			appearance = {
 				nerd_font_variant = "mono",
 			},
+
 			completion = {
 				documentation = { auto_show = false },
+				completeopt = "menu,menuone,noselect",
+				trigger = { prefetch_on_insert = false },
 			},
+
 			sources = {
-				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+				default = { "lazydev", "lsp", "path", "buffer", "snippets", "minuet" },
 				providers = {
 					lazydev = {
 						name = "LazyDev",
 						module = "lazydev.integrations.blink",
 						score_offset = 100,
 					},
+					snippets = {
+						name = "Snippets",
+						snippet_engine = "native",
+					},
+					minuet = {
+						name = "minuet",
+						module = "minuet.blink",
+						async = true,
+						-- Should match minuet.config.request_timeout * 1000,
+						-- since minuet.config.request_timeout is in seconds
+						timeout_ms = 3000,
+						score_offset = 50, -- Gives minuet higher priority among suggestions
+					},
 				},
 			},
+
 			fuzzy = { implementation = "prefer_rust_with_warning" },
 		},
-		opts_extend = { "sources.default" },
+		config = function(_, opts)
+			require("lazydev").setup()
+			require("blink.cmp").setup(opts)
+		end,
 	},
 }
