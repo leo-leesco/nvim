5feab77bb1900e7823a03761ad78136fdf3bdc4d
Léo LEESCO <leo.leesco@gmail.com>
2025-06-16 15:01:50 -0700

    remove build step

--- lua/plugins/web.lua
+++ lua/plugins/web.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"olrtg/nvim-emmet",
 		ft = { "html", "css", "javascript", "typescript", "astro" },
-		build = function()
-			vim.cmd("LspInstall emmet_language_server")
-		end,
 		config = function()
 			vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation)
 		end,
