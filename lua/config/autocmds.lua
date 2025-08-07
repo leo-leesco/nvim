d1a2a3af3d8472dad674754c231698d5ed1ca631
Léo LEESCO <leo.leesco@gmail.com>
2025-08-07 16:37:45 -0700

    use VimTex

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,30 +1,6 @@
 -- Add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
 
-local latex = vim.api.nvim_create_augroup("Latex", { clear = true })
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = latex,
-	pattern = "*.tex",
-	callback = function()
-		vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%'")
-
-		-- check for bibtex files
-		if vim.fn.glob(vim.fn.getcwd() .. "/*.bib") then
-			vim.print(".bib found")
-			vim.cmd("silent !bibtex '%:r' &")
-			vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%' &")
-			vim.cmd("silent !pdflatex -shell-escape -interaction=nonstopmode -output-directory '%:p:h' '%' &")
-		end
-
-		-- toggle Preview to make sure the PDF is re-rendered
-		vim.cmd("silent! !open '%:r.pdf'")
-		-- vim.cmd("silent! !open -g '%:r.pdf'")
-		-- vim.cmd [[ !osascript -e 'tell application "Preview" to open POSIX file "%:p:r.pdf"' -e 'tell application "Preview" to activate' -e 'tell application "System Events" to tell process "Preview" to keystroke "1" using {command down}' -e 'tell application "System Events" to tell process "Preview" to keystroke "9" using {command down}' -e 'tell application "System Events" to tell process "Preview" to key code 31 using {command down}' -e 'tell application "System Events" to tell process "Preview" to key code 124 using {command down}' ]]
-		vim.cmd("silent! !open -a wezterm.app")
-	end,
-})
-
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "markdown", "txt" },
 	callback = function()
@@ -66,7 +42,8 @@ vim.api.nvim_create_autocmd("User", {
 				lockfile,
 			}):wait()
 			if add_result.code ~= 0 then
-				vim.notify("git add failed:\n" .. (add_result.stderr or add_result.stdout), vim.log.levels.WARN)
+				vim.notify("git add failed:\n" .. (add_result.stderr or add_result.stdout),
+					vim.log.levels.WARN)
 				return
 			end
 
