e9193b053bb7a985cfa5c1dd796dfbb2ffd5f70c
Léo LEESCO <leo.leesco@gmail.com>
2025-08-09 12:45:58 -0700

    install texlab

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -40,6 +40,11 @@ lsp_clients = {
 		package_manager = "bun",
 		flags = "-g",
 	},
+	texlab = {
+		deps = { "https://github.com/latex-lsp/texlab" },
+		package_manager = "cargo",
+		flags = "--locked --tag v5.23.1 --git",
+	},
 }
 
 -- CUSTOM COMMANDS
@@ -158,8 +163,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		-- When you move your cursor, the highlights will be cleared (the second autocommand).
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
 		if
-		    client
-		    and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
+			client
+			and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
 		then
 			local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight",
 				{ clear = false })
@@ -182,7 +187,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 					vim.api.nvim_clear_autocmds({
 						group = "kickstart-lsp-highlight",
 						buffer = event2
-						    .buf
+							.buf
 					})
 				end,
 			})
