688992ff2864d4ef1e9f462bc00bffcc16f5a4c2
Léo LEESCO <leo.leesco@gmail.com>
2025-08-23 10:07:24 -0700

    prevent typst preview from stealing focus

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -2,5 +2,7 @@ return {
 	'chomosuke/typst-preview.nvim',
 	lazy = false, -- or ft = 'typst'
 	version = '1.*',
-	opts = {},  -- lazy.nvim will implicitly calls `setup {}`
+	opts = {
+		open_cmd = "open -g %s",
+	}, -- lazy.nvim will implicitly calls `setup {}`
 }
