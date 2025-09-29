5e9fd7225fd2b68c6396dcc6cabf1130dabf1a56
Léo LEESCO <leo.leesco@gmail.com>
2025-09-29 15:08:31 +0200

    start coq on ft

--- lua/plugins/coq.lua
+++ lua/plugins/coq.lua
@@ -1,10 +1,21 @@
 return {
 	'whonore/Coqtail',
+	filetype = { "coq", "*.v" },
 	config = function()
 		vim.g.coqtail_coq_path = "/opt/homebrew/bin/"
 		vim.cmd [[ "follow the colorscheme used"
 			hi def link CoqtailChecked DiffAdd
 			hi def link CoqtailSent    DiffChange
 		]]
+
+		vim.api.nvim_create_autocmd(
+			"FileType",
+			{
+				pattern = { "coq", "*.v" },
+				callback = function(_)
+					vim.fn.execute("RocqStart")
+				end,
+			}
+		)
 	end
 }
