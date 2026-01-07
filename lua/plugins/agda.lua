4e8aeb34bda4f06d80f3809f1f1ba0ef9a7e590e
Léo LEESCO <leo.leesco@gmail.com>
2026-01-07 23:57:58 +0100

    align (format)

--- lua/plugins/agda.lua
+++ lua/plugins/agda.lua
@@ -34,23 +34,23 @@ return {
 		vim.o.tabstop = 1
 		vim.o.softtabstop = 1
 		vim.o.shiftwidth = 1
-
 	end,
 	keys = {
-		{ "<leader>l", ":CornelisRestart<CR>",          ft = "agda" },
-		{ "<leader>r", ":CornelisRefine<CR>",           ft = "agda" },
-		{ "<leader>d", ":CornelisMakeCase<CR>",         ft = "agda" },
-		{ "<leader>,", ":CornelisTypeContext<CR>",      ft = "agda" },
-		{ "<leader>.", ":CornelisTypeContextInfer<CR>", ft = "agda" },
-		{ "<leader>n", ":CornelisSolve<CR>",            ft = "agda" },
-		{ "<leader>c", ":CornelisElaborate<CR>",        ft = "agda" },
-		{ "<leader>a", ":CornelisAuto<CR>",             ft = "agda" },
-		{ "gd",        ":CornelisGoToDefinition<CR>",   ft = "agda" },
-		{ "[d",        ":CornelisPrevGoal<CR>",         ft = "agda" },
-		{ "]d",        ":CornelisNextGoal<CR>",         ft = "agda" },
-		{ "<C-A>",     ":CornelisInc<CR>",              ft = "agda" },
-		{ "<C-X>",     ":CornelisDec<CR>",              ft = "agda" },
-		{ "<leader>?", ":CornelisQuestionToMeta<CR>",   ft = "agda" },
-		{ "<leader>P", ":CornelisHelperFunc<CR>",       ft = "agda" }, -- this copies the current goal to the yank register
+		{ "<leader>l",    ":CornelisRestart<CR>",          ft = "agda" },
+		{ "<leader>r",    ":CornelisRefine<CR>",           ft = "agda" },
+		{ "<leader>d",    ":CornelisMakeCase<CR>",         ft = "agda" },
+		{ "<leader>,",    ":CornelisTypeContext<CR>",      ft = "agda" },
+		{ "<leader>;",    ":CornelisTypeContextInfer<CR>", ft = "agda" },
+		{ "<leader><CR>", ":CornelisTypeInfer<CR>",        ft = "agda" },
+		{ "<leader>n",    ":CornelisSolve<CR>",            ft = "agda" },
+		{ "<leader>c",    ":CornelisElaborate<CR>",        ft = "agda" },
+		{ "<leader>a",    ":CornelisAuto<CR>",             ft = "agda" },
+		{ "gd",           ":CornelisGoToDefinition<CR>",   ft = "agda" },
+		{ "[d",           ":CornelisPrevGoal<CR>",         ft = "agda" },
+		{ "]d",           ":CornelisNextGoal<CR>",         ft = "agda" },
+		{ "<C-A>",        ":CornelisInc<CR>",              ft = "agda" },
+		{ "<C-X>",        ":CornelisDec<CR>",              ft = "agda" },
+		{ "<leader>?",    ":CornelisQuestionToMeta<CR>",   ft = "agda" },
+		{ "<leader>P",    ":CornelisHelperFunc<CR>",       ft = "agda" }, -- this copies the current goal to the yank register
 	}
 }
