7fce685e012ee218b2e1f3b0e163c52488366fd9
Léo LEESCO <leo.leesco@gmail.com>
2026-01-13 11:48:19 +0100

    add parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,62 @@
+languages = {
+				-- logic
+				"agda",
+
+				-- general purpose
+				"python",
+				"ocaml",
+				"ocamllex",
+
+				-- `nvim` config
+				"lua",
+				"luadoc",
+				"vim",
+				"vimdoc",
+
+				-- web dev
+				"astro",
+				"html",
+				"css",
+				"javascript",
+				"typescript",
+
+				-- markup
+				"markdown",
+				"latex",
+				"typst",
+
+				-- config
+				"yaml",
+				"json",
+
+				-- shell
+				"fish",
+			}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = {
+		vim.system({"bun", "install", "-g", "tree-sitter-cli"},
+		{ text = true }, function(obj)
+				vim.schedule(function()
+if obj.code == 0 then
+    vim.notify("Output: " .. obj.stdout, vim.log.levels.INFO)
+  else
+    vim.notify("Error: " .. obj.stderr, vim.log.levels.ERROR)
+  end
+				end)
+end),
+		":TSUpdate",
+	},
+	main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+	opts = {
+      ensure_installed = languages,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+		}
 }
