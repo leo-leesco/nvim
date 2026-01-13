2ed7393f9edc19e4a1f5817166a5e8a5b979f7fc
Léo LEESCO <leo.leesco@gmail.com>
2026-01-13 17:51:25 +0100

    lua LSP

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,2 +1,6 @@
+vim.lsp.enable("lua")
+vim.lsp.start(vim.lsp.config.lua)
+
+-- configure fallback formatter
 -- The "-" tells stylua to read from stdin
 vim.bo.formatprg = "stylua --search-parent-directories --stdin-filepath " .. vim.api.nvim_buf_get_name(0) .. " -"
