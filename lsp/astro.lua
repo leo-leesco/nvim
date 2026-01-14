4a400a0e25a2f43603953aa05fe875e7e20f3277
Léo LEESCO <leo.leesco@gmail.com>
2026-01-14 11:25:13 +0100

    formatting

--- lsp/astro.lua
+++ lsp/astro.lua
@@ -23,9 +23,11 @@ return {
 		"jsconfig.json",
 		".git",
 	},
+
 	init_options = {
 		typescript = {},
 	},
+
 	before_init = function(_, config)
 		if config.init_options and config.init_options.typescript and not config.init_options.typescript.tsdk then
 			config.init_options.typescript.tsdk = util.get_typescript_server_path(config.root_dir)
