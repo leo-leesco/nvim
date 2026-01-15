023540e232d1cd64a760b72653dcf67c7cb3f88e
Léo LEESCO <leo.leesco@gmail.com>
2026-01-15 16:31:47 +0100

    avoid compilation on non-compilable files

--- ftplugin/latex.lua
+++ ftplugin/latex.lua
@@ -1,4 +1,9 @@
 vim.lsp.enable("latex")
 vim.lsp.start(vim.lsp.config.latex)
 
-vim.cmd("VimtexCompile")
+local bufname = vim.api.nvim_buf_get_name(0)
+if bufname:match("%.tex$") then
+	vim.cmd("VimtexCompile")
+end
+
+vim.opt.formatprg = "latexindent"
