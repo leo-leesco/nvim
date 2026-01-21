36cd308941980a7f81f548a250de9e4a76b9d0f7
Léo LEESCO <leo.leesco@gmail.com>
2026-01-21 17:16:22 +0100

    haskell LSP

---
+++ ftplugin/haskell.lua
@@ -0,0 +1,2 @@
+vim.lsp.enable("haskell")
+vim.lsp.start(vim.lsp.config.haskell)
---
+++ lsp/haskell.lua
@@ -0,0 +1,35 @@
+---@brief
+---
+--- https://github.com/haskell/haskell-language-server
+---
+--- Haskell Language Server
+---
+--- If you are using HLS 1.9.0.0, enable the language server to launch on Cabal files as well:
+---
+--- ```lua
+--- vim.lsp.config('hls', {
+---   filetypes = { 'haskell', 'lhaskell', 'cabal' },
+--- })
+--- ```
+
+local util = require 'lspconfig.util'
+
+local server = 'haskell-language-server'
+local executable = server .. '-wrapper'
+require "ensure_installed" (server, "brew install" .. server)
+
+---@type vim.lsp.Config
+return {
+	cmd = { executable, '--lsp' },
+	filetypes = { 'haskell', 'lhaskell' },
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		on_dir(util.root_pattern('hie.yaml', 'stack.yaml', 'cabal.project', '*.cabal', 'package.yaml')(fname))
+	end,
+	settings = {
+		haskell = {
+			formattingProvider = 'ormolu',
+			cabalFormattingProvider = 'cabal-fmt',
+		},
+	},
+}
