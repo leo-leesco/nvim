4d08c6fd941008d6e543a71f29d124dadb165e0c
Léo LEESCO <leo.leesco@gmail.com>
2026-01-21 17:42:56 +0100

    fix callbacks

--- lua/ensure_installed.lua
+++ lua/ensure_installed.lua
@@ -1,7 +1,11 @@
 return function(executable, install_command)
-	if not vim.fn.executable(executable) and vim.fn.confirm("Install " .. executable .. " ?", "&Yes\n&No") then
+	-- vim.schedule(function()
+	if vim.fn.executable(executable) ~= 1 and vim.fn.confirm("Install " .. executable .. " ?", "&Yes\n&No") == 1 then
 		vim.system(install_command, { text = true }, function(obj)
-			require("log")(obj)
+			vim.schedule(function()
+				require("log")(obj)
+			end)
 		end)
 	end
+	-- end)
 end
--- lua/log.lua
+++ lua/log.lua
@@ -1,3 +1,3 @@
 return function(obj)
-	vim.fn.writefile(vim.inspect(obj), vim.env["NVIM_LOG_FILE"], "a")
+	vim.fn.writefile({ vim.inspect(obj) }, vim.env["NVIM_LOG_FILE"], "a")
 end
