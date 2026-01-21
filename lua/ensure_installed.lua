76ffe2a50627cc50ffbde2381956de023857aa3e
Léo LEESCO <leo.leesco@gmail.com>
2026-01-21 19:12:27 +0100

    move install in a dedicated shell

--- lua/ensure_installed.lua
+++ lua/ensure_installed.lua
@@ -1,11 +1,18 @@
+--- @param executable string
+--- @param install_command string[]
 return function(executable, install_command)
-	-- vim.schedule(function()
-	if vim.fn.executable(executable) ~= 1 and vim.fn.confirm("Install " .. executable .. " ?", "&Yes\n&No") == 1 then
-		vim.system(install_command, { text = true }, function(obj)
-			vim.schedule(function()
-				require("log")(obj)
+	if
+			vim.fn.executable(executable) ~= 1
+			and vim.fn.confirm("Install " .. executable .. " ?", "&Yes\n&No") == 1
+	then
+		vim.system(
+
+			{ "sh", "-c", table.concat(install_command, " ") },
+
+			{ text = true }, function(obj)
+				vim.schedule(function()
+					require("log")(obj)
+				end)
 			end)
-		end)
 	end
-	-- end)
 end
