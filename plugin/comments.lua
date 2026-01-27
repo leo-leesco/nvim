0887a579ec6999e083ded76a4edf95eda0682acf
Léo LEESCO <leo.leesco@gmail.com>
2026-01-27 10:13:53 +0100

    rename

--- plugin/comments.lua
+++ plugin/comments.lua
@@ -1,19 +1,19 @@
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "*",
 	callback = function()
-		local escaped_comment_string = vim.fn.escape(vim.bo.commentstring:gsub("%%s", ""):gsub("%s+$", ""), "\\\\/*^$.~[]")
+		local escaped_commentstring = vim.fn.escape(vim.bo.commentstring:gsub("%%s", ""):gsub("%s+$", ""), "\\\\/*^$.~[]")
 
 		vim.keymap.set({ "n", "v" }, "}", function()
 			vim.cmd.normal { bang = true, "0" }
-			vim.fn.search("\\v^(" .. escaped_comment_string .. ")?[^" .. escaped_comment_string:sub(1, 1) .. "]", "cW")
-			vim.fn.search("\\v(^(" .. escaped_comment_string .. ")?$|%$)", "W")
+			vim.fn.search("\\v^(" .. escaped_commentstring .. ")?[^" .. escaped_commentstring:sub(1, 1) .. "]", "cW")
+			vim.fn.search("\\v(^(" .. escaped_commentstring .. ")?$|%$)", "W")
 		end
 		)
 
 		vim.keymap.set({ "n", "v" }, "{", function()
 			vim.cmd.normal { bang = true, "0" }
-			vim.fn.search("\\v^(" .. escaped_comment_string .. ")?[^" .. escaped_comment_string:sub(1, 1) .. "]", "bcW")
-			vim.fn.search("\\v(^(" .. escaped_comment_string .. ")?$|%$)", "bW")
+			vim.fn.search("\\v^(" .. escaped_commentstring .. ")?[^" .. escaped_commentstring:sub(1, 1) .. "]", "bcW")
+			vim.fn.search("\\v(^(" .. escaped_commentstring .. ")?$|%$)", "bW")
 		end
 		)
 	end
