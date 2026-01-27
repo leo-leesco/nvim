a6b0c683c4a8a4cf9f3bd180817e869cdada32c5
Léo LEESCO <leo.leesco@gmail.com>
2026-01-27 10:05:17 +0100

    general solution to moving through comments

--- ftplugin/help.vim
+++ ftplugin/help.vim
@@ -5,7 +5,3 @@ else
 endif
 
 set nowrapscan
-
-" make }{ work even on commented paragraphs
-map g} <Cmd>normal! 0<Bar>call search('\v^"?[^"]', 'cW')<Bar>call search('\v(^"?$\|%$)', 'W')<CR>
-map g{ <Cmd>normal! 0<Bar>call search('\v^"?[^"]', 'bcW')<Bar>call search('\v(^"?$\|%^)', 'bW')<CR>
--- plugin/comments.lua
+++ plugin/comments.lua
@@ -1,18 +1,20 @@
-local escaped_comment_string = vim.fn.escape(
-	vim.bo.commentstring:gsub("%%s", ""):gsub("%s+$", ""),
-	"\\\\/*^$.~[]"
-)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function()
+		local escaped_comment_string = vim.fn.escape(vim.bo.commentstring:gsub("%%s", ""):gsub("%s+$", ""), "\\\\/*^$.~[]")
 
-vim.keymap.set({ "n", "v" }, "}", function()
-	vim.cmd.normal { bang = true, "0" }
-	vim.fn.search('\\v^(' .. escaped_comment_string .. ')@!', "cW")
-	vim.fn.search('\\v(^' .. escaped_comment_string .. '$|%$)', "W")
-end
-)
+		vim.keymap.set({ "n", "v" }, "}", function()
+			vim.cmd.normal { bang = true, "0" }
+			vim.fn.search("\\v^(" .. escaped_comment_string .. ")?[^" .. escaped_comment_string:sub(1, 1) .. "]", "cW")
+			vim.fn.search("\\v(^(" .. escaped_comment_string .. ")?$|%$)", "W")
+		end
+		)
 
-vim.keymap.set({ "n", "v" }, "{", function()
-	vim.cmd.normal { bang = true, "0" }
-	vim.fn.search("\\v^\"?[^\"]", "bcW")
-	vim.fn.search("\\v(^\"?$|%^)", "bW")
-end
-)
+		vim.keymap.set({ "n", "v" }, "{", function()
+			vim.cmd.normal { bang = true, "0" }
+			vim.fn.search("\\v^(" .. escaped_comment_string .. ")?[^" .. escaped_comment_string:sub(1, 1) .. "]", "bcW")
+			vim.fn.search("\\v(^(" .. escaped_comment_string .. ")?$|%$)", "bW")
+		end
+		)
+	end
+})
