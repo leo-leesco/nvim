bec589b26698ab453dad7f51bee71d8dc340309a
Léo LEESCO <leo.leesco@gmail.com>
2026-02-03 13:11:56 +0100

    smart configure ocaml makeprg

--- ftplugin/ocaml.lua
+++ ftplugin/ocaml.lua
@@ -1,2 +1,19 @@
 vim.lsp.enable("ocaml")
 vim.lsp.start(vim.lsp.config.ocaml)
+
+-- `makeprg` options
+vim.cmd.compiler = "ocaml"
+local dune_markers = { "dune-project", "dune" }
+
+local found = vim.fs.find(dune_markers, {
+	upward = true,
+	-- stop = vim.env.HOME, -- Stop searching at home to avoid slow disks
+	path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)),
+	limit = 1, -- We only need to know if ONE exists
+})
+
+if #found > 0 then
+	vim.bo.makeprg = "dune $*"
+else
+	vim.bo.makeprg = "ocamlopt % -o %:r"
+end
