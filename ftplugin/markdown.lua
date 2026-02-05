b6a18e9bfc2ae4347d7d88b3dc9b741f9aab772b
Léo LEESCO <leo.leesco@gmail.com>
2026-02-05 14:27:33 +0100

    documentation

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,11 @@
+vim.g.pandoc_compiler_args = '--toc'
+vim.cmd.compiler("pandoc") -- WARNING : the standard behaviour of `compiler-pandoc` has been edited : the output format is `pdf` ; passing options to `:make` does not make any difference
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+	pattern = "make",
+	callback = function()
+		if vim.v.shell_error ~= 0 then return end
+
+		local output_file = vim.fn.expand("%:r") .. ".pdf"
+		vim.ui.open(output_file)
+	end,
+})
--- ftplugin/markdown.vim
+++
@@ -1,2 +0,0 @@
-let g:pandoc_compiler_args='--toc'
-compiler pandoc
