523b9fa1e57bcafebbef4d1a1d7369066fa7c199
Léo LEESCO <leo.leesco@gmail.com>
2026-02-10 22:21:13 +0100

    configure lazydev
    attempt 2 : lazydev-specific blink config

---
+++ lua/plugins/nvim-lsp.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		opts = {
+			sources = {
+				per_filetype = {
+					lua = {
+						inherit_defaults = true, 'lazydev' }
+				},
+				providers = {
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						-- make lazydev completions top priority (see `:h blink.cmp`)
+						score_offset = 100,
+					},
+				},
+			},
+		},
+	}
+}
