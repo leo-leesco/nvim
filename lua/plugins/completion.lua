8d9a0d1ccfb1f18af2a0bfc9b67962852c1a5a19
Léo LEESCO <leo.leesco@gmail.com>
2026-02-10 15:26:55 +0100

    base install of blink.cmp

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,29 @@
+return {
+	'saghen/blink.cmp',
+	dependencies = { 'rafamadriz/friendly-snippets' },
+
+	build = 'cargo build --release',
+
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = { preset = 'default' },
+
+		appearance = {
+			nerd_font_variant = 'mono'
+		},
+
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = { documentation = { auto_show = false } },
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" }
+	},
+	opts_extend = { "sources.default" }
+}
