ccf46daa807ef35609bef52bb13442c740817b92
Léo LEESCO <leo.leesco@gmail.com>
2026-02-10 15:26:38 +0100

    fix trouble not closing

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -1,3 +1,17 @@
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("TroubleAutoClose", { clear = true }),
+	callback = function()
+		local wins = vim.api.nvim_list_wins()
+
+		if #wins == 1 then
+			local buf = vim.api.nvim_win_get_buf(wins[1])
+			if vim.bo[buf].filetype == "trouble" then
+				vim.cmd.quit()
+			end
+		end
+	end,
+})
+
 return {
 	"folke/trouble.nvim",
 	event = "LspAttach",
