39a542662bb90aacfd58a56d945a530cda4d1588
Léo LEESCO <leo.leesco@gmail.com>
2026-02-17 09:36:54 +0100

    change default options
    process latex macros

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,4 +1,4 @@
-vim.g.pandoc_compiler_args = '--toc'
+vim.g.pandoc_compiler_args = '--latex_macros'
 vim.cmd.compiler("pandoc") -- WARNING : the standard behaviour of `compiler-pandoc` has been edited : the output format is `pdf` ; passing options to `:make` does not make any difference
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
 	pattern = "make",
