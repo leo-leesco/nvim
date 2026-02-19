d7647d75aff1a093b141c35d890a0ff7e54d3d0b
Léo LEESCO <leo.leesco@gmail.com>
2026-02-19 13:14:30 +0100

    fix double prompting

--- lua/ensure_installed.lua
+++ lua/ensure_installed.lua
@@ -1,16 +1,13 @@
-if not vim.g.ensure_installed then
-	vim.g.ensure_installed = {}
-end
+--- it is created once per `nvim` session, and is safely accessed only by this script
+local ensure_installed = {}
 
---- this function produces a global `vim.g.ensure_installed` table ; it is used to make sure that in a given session, this function is never called more than once (for instance the user has refused the prompt)
 --- @param executable string
 --- @param install_command string[]
 return function(executable, install_command)
-	if vim.g.ensure_installed[executable] then
+	if ensure_installed[executable] then
 		return
-	else
-		vim.g.ensure_installed[executable] = true
 	end
+	ensure_installed[executable] = true
 
 	if
 			vim.fn.executable(executable) ~= 1
