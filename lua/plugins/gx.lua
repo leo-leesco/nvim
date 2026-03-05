edb0d33733f9cdca40a61c89f22b1c7aede2e2cc
Léo LEESCO <leo.leesco@gmail.com>
2026-03-05 14:37:07 +0100

    better behaviour for gx (search online)

---
+++ lua/plugins/gx.lua
@@ -0,0 +1,44 @@
+return {
+	"chrishrb/gx.nvim",
+	keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+	-- cmd = { "Browse" },
+	init = function()
+		vim.g.netrw_nogx = 1 -- disable netrw gx
+	end,
+
+	opts = {
+		select_prompt = true, -- shows a prompt when multiple handlers match; disable to auto-select the top one
+
+		handlers = {
+			plugin = true,    -- open plugin links in lua (e.g. packer, lazy, ..)
+			github = true,    -- open github issues
+			brewfile = false, -- open Homebrew formulaes and casks
+			package_json = true, -- open dependencies from package.json
+			search = false,   -- search the web/selection on the web if nothing else is found
+			go = false,       -- open pkg.go.dev from an import statement (uses treesitter)
+
+			-- jira = { -- custom handler to open Jira tickets (these have higher precedence than builtin handlers)
+			--   name = "jira", -- set name of handler
+			--   handle = function(mode, line, _)
+			--     local ticket = require("gx.helper").find(line, mode, "(%u+-%d+)")
+			--     if ticket and #ticket < 20 then
+			--       return "http://jira.company.com/browse/" .. ticket
+			--     end
+			--   end,
+			-- },
+
+			rust = {               -- custom handler to open rust's cargo packages
+				name = "rust",       -- set name of handler
+				filetype = { "toml" }, -- you can also set the required filetype for this handler
+				filename = "Cargo.toml", -- or the necessary filename
+				handle = function(mode, line, _)
+					local crate = require("gx.helper").find(line, mode, "(%w+)%s-=%s")
+
+					if crate then
+						return "https://crates.io/crates/" .. crate
+					end
+				end,
+			},
+		},
+	}
+}
