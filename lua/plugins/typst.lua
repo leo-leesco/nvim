1c485aa317c87a1ed831462c5124bb323c4fccb3
Léo LEESCO <leo.leesco@gmail.com>
2026-03-05 15:05:19 +0100

    add install script

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -2,6 +2,10 @@ return {
 	'chomosuke/typst-preview.nvim',
 	ft = "typst",
 	version = '1.*',
+	build = { function()
+		local executable = "typst"
+		require("ensure_installed")(executable, { "brew install", executable })
+	end },
 	opts = {
 		open_cmd = "open -g %s",
 	}
