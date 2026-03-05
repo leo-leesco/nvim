075d73daeaece10e47c3fdbe8f8b55ec6fb968d9
Léo LEESCO <leo.leesco@gmail.com>
2026-03-05 14:42:30 +0100

    fix keymap

--- lua/plugins/gx.lua
+++ lua/plugins/gx.lua
@@ -1,6 +1,6 @@
 return {
 	"chrishrb/gx.nvim",
-	keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+	keys = { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } },
 	cmd = { "Browse" },
 	init = function()
 		vim.g.netrw_nogx = 1 -- disable netrw gx
@@ -10,14 +10,14 @@ return {
 		select_prompt = false, -- shows a prompt when multiple handlers match; disable to auto-select the top one
 
 		handlers = {
+			-- built-in handlers
 			plugin = true,         -- open plugin links in lua (e.g. packer, lazy, ..)
 			github = true,         -- open github issues
 			package_json = true,   -- open dependencies from package.json
 
-			rust = {               -- custom handler to open rust's cargo packages
-				name = "rust",       -- set name of handler
-				filetype = { "toml" }, -- you can also set the required filetype for this handler
-				filename = "Cargo.toml", -- or the necessary filename
+			rust = {               -- custom handler
+				name = "rust",
+				filename = "Cargo.toml", -- you can also set the required filetype for this handler via `filetype`
 				handle = function(mode, line, _)
 					local crate = require("gx.helper").find(line, mode, "(%w+)%s-=%s")
 
@@ -27,9 +27,9 @@ return {
 				end,
 			},
 
-			brewfile = false, -- open Homebrew formulaes and casks
-			search = false, -- search the web/selection on the web if nothing else is found
-			go = false,    -- open pkg.go.dev from an import statement (uses treesitter)
+			brewfile = false,
+			search = false, -- no online search
+			go = false,
 		},
 	}
 }
