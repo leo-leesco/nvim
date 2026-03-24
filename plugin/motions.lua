40a5d9c246bb002e5dcd68e08522b66aa640ede3
Léo LEESCO <leo.leesco@gmail.com>
2026-03-24 18:56:28 +0100

    try to fix comment detection

--- plugin/motions.lua
+++ plugin/motions.lua
@@ -1,7 +1,10 @@
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "*",
 	callback = function()
-		local escaped_commentstring = "\\V" .. vim.bo.commentstring:gsub("%%s", ""):gsub("%s+$", "") .. "\\v"
+		local escaped_commentstring = "\\V" ..
+				vim.bo.commentstring:sub(0, (vim.bo.commentstring:find("%s", 1, true) or 0) - 1):gsub("%s+$", "")
+				.. "\\v"
+		vim.print(escaped_commentstring)
 
 		vim.keymap.set({ "n", "v" }, "}", function()
 			vim.cmd.normal { bang = true, "0" }
