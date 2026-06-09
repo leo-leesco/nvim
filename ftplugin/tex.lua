bc08404d879864656137e77aeb24245e1830c987
Léo LEESCO <leo.leesco@gmail.com>
2026-06-09 15:47:25 -0400

    compile tex files only when vimtex is fully loaded

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -1,6 +1,13 @@
-local bufname = vim.api.nvim_buf_get_name(0)
-if bufname:match("%.tex$") then
-	vim.cmd("VimtexCompile")
+-- wait for VimTex to be fully loaded before trying to compile
+if vim.api.nvim_buf_get_name(0):match("%.tex$") then
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "VimtexEventInitPost",
+		callback = function(ev)
+			if ev.buf ~= vim.api.nvim_get_current_buf() then return end
+			vim.cmd("VimtexCompile")
+			return true -- delete the autocmd once it has run for our buffer
+		end,
+	})
 end
 
 vim.opt.formatprg = "latexindent"
