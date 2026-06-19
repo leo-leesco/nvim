9155b8783927803e886da182e7b61a4771198e38
Léo LEESCO <leo.leesco@gmail.com>
2026-06-19 13:28:21 -0400

    colorscheme updates on setup and restart

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,18 +14,28 @@ fd:start(appearance_file, {}, vim.schedule_wrap(set_background))
 
 return {
 	"EdenEast/nightfox.nvim",
+	priority = 1000,
 	config = function()
+		local apply = function()
+			if vim.o.background == "light" then
+				vim.cmd.colorscheme("dayfox")
+			else
+				vim.cmd.colorscheme("nightfox")
+			end
+		end
+
+		-- react to later light/dark switches while nvim is running
 		vim.api.nvim_create_autocmd("OptionSet", {
 			pattern = "background",
 			callback = function()
-				local bg = vim.o.background
-				require "log" ("Background changed to: " .. bg)
-				if bg == "light" then
-					vim.cmd.colorscheme("dayfox")
-				else
-					vim.cmd.colorscheme("nightfox")
-				end
+				require "log" ("Background changed to: " .. vim.o.background)
+				apply()
 			end,
 		})
+
+		-- the fs_event above only fires on *changes*, so read the file
+		-- and apply the matching colorscheme once at startup
+		set_background()
+		apply()
 	end,
 }
